d185024ba781ad7505b7be571017a829d59a489a
royaltransfers <royaltransfers.acc@gmail.com>
2025-08-06 15:31:17 +0500

    update: skeleton

--- src/app/components/LocationQuoteFormSkeleton.tsx
+++ src/app/components/LocationQuoteFormSkeleton.tsx
@@ -1,13 +1,20 @@
-import { LocationMark, LocationPin } from "@/assets";
+import {
+  LocationMark,
+  LocationPin,
+  PassengerIcon,
+  LargeSeatIcon,
+  OneWayActive,
+  TwoWayNotActive,
+} from "@/assets";
 import Image from "next/image";
 
 export default function LocationQuoteFormSkeleton() {
   return (
-    <div className="w-2/5 flex items-end justify-center h-full">
-      <div className="w-11/12 px-4 py-6 xl:px-8 xl:py-14 bg-white z-10 rounded-2xl flex flex-col gap-4 xl:gap-6 poppins animate-pulse">
-        {/* Pickup Field */}
-        <div className="flex flex-col gap-1 xl:gap-3">
-          <label className="text-lg xl:text-2xl uppercase bebas-neue leading-none">
+    <div className="lg:w-2/5 2xl:w-[35%] flex items-end justify-center h-full lg:pb-8 xl:pb-16">
+      <div className="w-full 2xl:w-11/12 px-4 py-6 xl:px-8 lg:py-10 bg-white z-10 rounded-2xl flex flex-col gap-4 xl:gap-6 poppins animate-pulse">
+        {/* Pickup */}
+        <div className="flex flex-col gap-1 lg:gap-3">
+          <label className="text-lg lg:text-xl xl:text-2xl uppercase bebas-neue leading-none">
             Pick Up Location
           </label>
           <div className="relative">
@@ -19,12 +26,7 @@ export default function LocationQuoteFormSkeleton() {
                 height={15}
               />
             </div>
-            <input
-              disabled
-              type="text"
-              placeholder="Loading..."
-              className="w-full px-8 py-2 xl:py-4 border border-gray-300 rounded-lg bg-gray-200 cursor-not-allowed"
-            />
+            <div className="w-full h-12 lg:h-14 bg-gray-200 rounded-lg px-8 py-2 cursor-not-allowed" />
             <div className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-300">
               <Image
                 src={LocationMark}
@@ -36,9 +38,9 @@ export default function LocationQuoteFormSkeleton() {
           </div>
         </div>
 
-        {/* Arrival Field */}
-        <div className="flex flex-col gap-1 xl:gap-3">
-          <label className="text-lg xl:text-2xl uppercase bebas-neue leading-none">
+        {/* Arrival */}
+        <div className="flex flex-col gap-1 lg:gap-3">
+          <label className="text-lg lg:text-xl xl:text-2xl uppercase bebas-neue leading-none">
             Arrival Location
           </label>
           <div className="relative">
@@ -50,22 +52,68 @@ export default function LocationQuoteFormSkeleton() {
                 height={15}
               />
             </div>
-            <input
-              disabled
-              type="text"
-              placeholder="Loading..."
-              className="w-full px-8 py-2 xl:py-4 border border-gray-300 rounded-lg bg-gray-200 cursor-not-allowed"
+            <div className="w-full h-12 lg:h-14 bg-gray-200 rounded-lg px-8 py-2 cursor-not-allowed" />
+          </div>
+        </div>
+
+        {/* Passengers & Luggage */}
+        <div className="flex gap-5 justify-between">
+          <div className="flex-1 flex flex-col gap-1 lg:gap-3">
+            <label className="text-lg lg:text-xl xl:text-2xl uppercase bebas-neue leading-none">
+              No Of Passengers
+            </label>
+            <div className="relative">
+              <div className="absolute left-3 top-1/2 -translate-y-1/2">
+                <Image
+                  src={PassengerIcon}
+                  alt="Passenger Icon"
+                  width={20}
+                  height={20}
+                />
+              </div>
+              <div className="w-full h-12 lg:h-14 bg-gray-200 rounded-lg px-8 py-2 cursor-not-allowed" />
+            </div>
+          </div>
+
+          <div className="flex-1 flex flex-col gap-1 lg:gap-3">
+            <label className="text-lg lg:text-xl xl:text-2xl uppercase bebas-neue leading-none">
+              No Of Luggage
+            </label>
+            <div className="relative">
+              <div className="absolute left-3 top-1/2 -translate-y-1/2">
+                <Image
+                  src={LargeSeatIcon}
+                  alt="Luggage Icon"
+                  width={20}
+                  height={20}
+                />
+              </div>
+              <div className="w-full h-12 lg:h-14 bg-gray-200 rounded-lg px-8 py-2 cursor-not-allowed" />
+            </div>
+          </div>
+        </div>
+
+        {/* Trip Type */}
+        <div className="flex flex-col items-start gap-2 my-2">
+          <p className="text-lg lg:text-xl uppercase bebas-neue">Trip Type</p>
+          <div className="flex gap-4 items-center">
+            <Image
+              src={OneWayActive}
+              alt="One Way Active"
+              className="w-[28px] h-[28px] lg:w-[34px] lg:h-[34px] 2xl:w-[40px] 2xl:h-[40px]"
             />
+            <div className="h-4 w-20 bg-gray-200 rounded" />
+            <Image
+              src={TwoWayNotActive}
+              alt="Two Way Not Active"
+              className="w-[28px] h-[28px] lg:w-[34px] lg:h-[34px] 2xl:w-[40px] 2xl:h-[40px]"
+            />
+            <div className="h-4 w-24 bg-gray-200 rounded" />
           </div>
         </div>
 
         {/* Submit Button */}
-        <button
-          disabled
-          className="bg-gray-300 text-white text-xl font-semibold px-8 py-3 rounded-full w-full cursor-not-allowed"
-        >
-          Loading...
-        </button>
+        <div className="h-12 lg:h-14 bg-gray-300 rounded-full w-full" />
       </div>
     </div>
   );
